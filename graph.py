f5c1bbfa2719f3b8df46e10e300018da499c38db
Aidan Fitzgerald <duckjr159@gmail.com>
2016-04-26 13:30:55 -0400

    Implementing the state machine logic first

--- graph.py
+++ graph.py
@@ -27,10 +27,41 @@ class DirectedGraph:
     The label, if any, must be hashable (i.e. have a __hash__(self) method).
     '''
     def add_edge(self, from_index, to_index, label=None):
+        # A tuple is hashable if and only if all of its elements are hashable. So this tuple can be added to a set.
+        # Using modulo to keep the to_index within range (list indexing takes care of from_index automatically).
         self.edges[from_index].add( (to_index % len(self.vertices), label) )
 
     '''
-    Returns the degree of the vertex (the number of edges leading from the vertex).
+    Returns the outdegree of the vertex (the number of edges leading from the vertex).
     '''
-    def degree(self, index):
+    def outdegree(self, index):
         return len(self.edges[from_index])
+
+    '''
+    Returns the indegree of the vertex (the number of edges leading to the vertex).
+    '''
+    def indegree(self, index):
+        pass
+
+    '''
+    Appends a graph to this graph, adding a directed edge from the from_index'th vertex of this graph to the to_index'th vertex of the other graph.
+    '''
+    def append(self, graph, from_index, to_index=0, label=None):
+        augend = len(self.vertices)
+        self.vertices += graph.vertices
+        for src_from_index in range(len(graph.edges)):
+            for src_to_index, label in graph.edges[i]:
+                self.add_edge(src_from_index + augend, src_to_index + augend, label)
+        self.add_edge(from_index, to_index + augend, label)
+
+    '''
+    Returns the value of the index'th vertex.
+    '''
+    def vertex(self, index):
+        return self.vertices[index]
+
+    '''
+    Returns the index of the first vertex having the given value.
+    '''
+    def index(self, value):
+        return self.vertices.index(value)
--- regex.py
+++ regex.py
@@ -1,6 +1,25 @@
 from graph import DirectedGraph
 
+def zero_or_one(graph):
+    pass
+
+def zero_or_more(graph):
+    pass
+
+def one_or_more(graph):
+    pass
+
+'''
+A state machine that matches strings to a regular expression.
+'''
 class Regex:
+
+    symbols = {
+        '?': zero_or_one,
+        '*': zero_or_more, # Kleene star
+        '+': one_or_more
+    }
+
     def __init__(self, regex):
         self.regex = regex
         # Vertices labelled with booleans, edges labelled with character classes (sets of zero or more characters)
@@ -11,4 +30,18 @@ class Regex:
         pass
 
     def match(self, string):
-        pass
+        state = 0
+        strindex = 0
+        # Negative state number means we've left the graph
+        while state >= 0 and strindex < len(string):
+            char = string[strindex]
+            # TODO add support for empty string transitions
+            empty_string_transitions = set()
+            for dest, label in self.graph.edges:
+                if char == label or char in label:
+                    state = dest
+                else:
+                    state = -1
+            strindex += 1
+            pass
+        return self.graph.vertex(state)
